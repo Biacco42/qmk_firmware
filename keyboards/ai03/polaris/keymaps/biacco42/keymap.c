3773b05d50e18fa6c0b17e85e3e329313f9f862f
Biacco42 <biacco42@gmail.com>
2021-05-21 00:47:56 +0900

    Fix 1 position

--- keyboards/ai03/polaris/keymaps/biacco42/keymap.c
+++ keyboards/ai03/polaris/keymaps/biacco42/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,
     S(KC_1), S(KC_2), S(KC_3), S(KC_4), S(KC_5), S(KC_6), S(KC_7), S(KC_8), S(KC_9), S(KC_0), S(KC_MINS), S(KC_EQL), _______,     _______,
     _______, KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,                    _______,
-    _______, KC_1,    _______, _______, KC_MHEN, KC_HENK, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,          _______, _______,
+    _______, _______, KC_1,    _______, KC_MHEN, KC_HENK, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,          _______, _______,
     _______, _______, _______,          _______,          _______,          _______,                   _______, _______, _______, RESET
   )
 };
